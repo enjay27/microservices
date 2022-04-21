4021b3cdf3a4bba0a39a6f3b9d4b89845c8b9ccf
Noah <23338687+enjay27@users.noreply.github.com>
2022-04-21 11:41:09 +0900

    UserService yml 의 value 를 가져오는 두 가지 방법. VO 사용 및 Environment 사용

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,13 +1,28 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
+
+    private final Environment environment;
+    private final Greeting greeting;
 
     @GetMapping("/health-check")
     public String status() {
         return "It's Working in User Service.";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        log.info(greeting.getMessage());
+        return environment.getProperty("greeting.message");
     }
 }
