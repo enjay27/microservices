4ddd446abbf22cd8390de91107acf251cf5334e8
Noah <23338687+enjay27@users.noreply.github.com>
2022-04-25 01:14:52 +0900

    URI 오타 수정 및 엔티티 테이블 이름 변경

--- order-service/src/main/java/com/example/orderservice/jpa/OrderEntity.java
+++ order-service/src/main/java/com/example/orderservice/jpa/OrderEntity.java
@@ -9,7 +9,7 @@ import java.util.Date;
 
 @Entity
 @Data
-@Table(name = "order")
+@Table(name = "orders")
 public class OrderEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
