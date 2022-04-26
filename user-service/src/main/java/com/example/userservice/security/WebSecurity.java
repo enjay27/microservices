79c98cbba05be00650eb55fe97bf42d01578f3c8
Noah <23338687+enjay27@users.noreply.github.com>
2022-04-26 13:38:52 +0900

    bootstrap 의 refresh, health, beans 기능 사용

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -21,6 +21,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
                 .hasIpAddress("192.168.50.248")
                 .and()
